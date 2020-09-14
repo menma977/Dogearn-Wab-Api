d762c3d75d6f7a5a7a3fd3482f105f5d098bb623
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-09-14 13:06:11 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,9 @@ use Carbon\Carbon;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
 class HomeController extends Controller
@@ -31,8 +33,9 @@ class HomeController extends Controller
    */
   public function index()
   {
+
     $lot = User::where('status', 2)->orderBy('level', 'asc')->get()->groupBy(function ($item) {
-      return "LOT ".$item->level;
+      return "LOT " . $item->level;
     })->map(function ($item) {
       $item->total = $item->count();
       return $item;
@@ -50,9 +53,60 @@ class HomeController extends Controller
       return $item;
     });
 
+    $dataFrom = Carbon::now();
+    $dateNow = Carbon::now();
+
     $data = [
       'graphicGroup' => $graphicGroup,
-      'lot' => $lot
+      'lot' => $lot,
+      'dateFrom' => $dataFrom,
+      'dateNow' => $dateNow
+    ];
+
+    return view('home', $data);
+  }
+
+  /**
+   * Show the application dashboard.
+   *
+   * @param Request $request
+   * @return Renderable
+   * @throws ValidationException
+   */
+  public function find(Request $request)
+  {
+    $this->validate($request, [
+      'dateFrom' => 'required|date',
+      'dateNow' => 'required|date',
+    ]);
+
+    $dateFrom = Carbon::parse($request->dateFrom);
+    $dateTo = Carbon::parse($request->dateNow);
+
+    $lot = User::where('status', 2)->whereBetween('created_at', [$dateFrom, $dateTo->addDay()])->orderBy('level', 'asc')->get()->groupBy(function ($item) {
+      return "LOT " . $item->level;
+    })->map(function ($item) {
+      $item->total = $item->count();
+      return $item;
+    });
+
+    $graphic = GradeHistory::whereBetween('created_at', [$dateFrom, $dateTo->addDay()])->where('credit', 0)->get();
+    $graphicGroup = $graphic->groupBy(function ($item) {
+      return (string)Carbon::parse($item->created_at)->format('d');
+    })->map(function ($item) {
+      $item->upgrade = $item->count();
+      $item->newUser = 0;
+      foreach ($item as $subItem) {
+        $item->newUser = User::whereDay('created_at', $subItem->created_at)->count();
+      }
+      return $item;
+    });
+
+    $data = [
+      'graphicGroup' => $graphicGroup,
+      'lot' => $lot,
+      'dateFrom' => $dateFrom,
+      'dateNow' => $dateTo
     ];
 
     return view('home', $data);
@@ -76,11 +130,15 @@ class HomeController extends Controller
   }
 
   /**
+   * @param $dateFrom
+   * @param $dateNow
    * @return Application|Factory|View|int
    */
-  public function newUserView()
+  public function newUserView($dateFrom, $dateNow)
   {
-    $dataUser = User::whereDay('created_at', Carbon::now())->get();
+    $dateFrom = Carbon::parse($dateFrom);
+    $dateTo = Carbon::parse($dateNow);
+    $dataUser = User::whereBetween('created_at', [$dateFrom, $dateTo->addDay()])->get();
     $dataUser->map(function ($item) {
       $binary = Binary::where('down_line', $item->id)->first();
       $item->sponsor = User::find($binary->sponsor);
@@ -91,6 +149,27 @@ class HomeController extends Controller
     ];
 
     return view('newUser', $data);
+  }
+
+  /**
+   * @param $dateFrom
+   * @param $dateNow
+   * @return Application|Factory|View
+   */
+  public function totalUpgradeView($dateFrom, $dateNow)
+  {
+    $dateFrom = Carbon::parse($dateFrom);
+    $dateTo = Carbon::parse($dateNow);
+    $dataUser = GradeHistory::whereBetween('created_at', [$dateFrom, $dateTo->addDay()])->where('credit', 0)->get();
+    $dataUser->map(function ($item) {
+      $item->user = User::find($item->user_id);
+    });
+
+    $data = [
+      'data' => $dataUser
+    ];
+
+    return view('totalLot', $data);
   }
 
   /**
@@ -105,23 +184,6 @@ class HomeController extends Controller
     ];
 
     return view('notActiveUser', $data);
-  }
-
-  /**
-   * @return Application|Factory|View
-   */
-  public function totalUpgradeView()
-  {
-    $dataUser = GradeHistory::whereDay('created_at', Carbon::now())->where('credit', 0)->get();
-    $dataUser->map(function ($item) {
-      $item->user = User::find($item->user_id);
-    });
-
-    $data = [
-      'data' => $dataUser
-    ];
-
-    return view('totalLot', $data);
   }
 
   /**
@@ -141,11 +203,27 @@ class HomeController extends Controller
   }
 
   /**
+   * @param $dateFrom
+   * @param $dateNow
    * @return int
    */
-  public function newUser()
+  public function newUser($dateFrom, $dateNow)
   {
-    return User::whereDay('created_at', Carbon::now())->get()->count();
+    $dateFrom = Carbon::parse($dateFrom);
+    $dateTo = Carbon::parse($dateNow);
+    return User::whereBetween('created_at', [$dateFrom, $dateTo->addDay()])->get()->count();
+  }
+
+  /**
+   * @param $dateFrom
+   * @param $dateNow
+   * @return mixed
+   */
+  public function totalUpgrade($dateFrom, $dateNow)
+  {
+    $dateFrom = Carbon::parse($dateFrom);
+    $dateTo = Carbon::parse($dateNow);
+    return GradeHistory::whereBetween('created_at', [$dateFrom, $dateTo->addDay()])->where('credit', 0)->count();
   }
 
   /**
@@ -154,10 +232,5 @@ class HomeController extends Controller
   public function newUserNotVerified()
   {
     return User::where('status', 0)->get()->count();
-  }
-
-  public function totalUpgrade()
-  {
-    return GradeHistory::whereDay('created_at', Carbon::now())->where('credit', 0)->count();
   }
 }
