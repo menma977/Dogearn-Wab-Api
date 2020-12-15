1d1d293967396c3d7c01e506e07d1b92c2395259
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-12-15 16:39:46 +0700

    Update 2020_08_15_081054_create_withdraw_queues_table.php

--- database/migrations/2020_08_15_081054_create_withdraw_queues_table.php
+++ database/migrations/2020_08_15_081054_create_withdraw_queues_table.php
@@ -6,32 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateWithdrawQueuesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('withdraw_queues', function (Blueprint $table) {
-            $table->id();
-            $table->integer('user_id');
-            $table->integer('send_to');
-            $table->text('send_value');
-            $table->text('total');
-            $table->integer('status');
-            $table->integer('type')->default(1);
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('withdraw_queues', function (Blueprint $table) {
+      $table->id();
+      $table->integer('user_id');
+      $table->integer('send_to');
+      $table->text('send_value');
+      $table->text('total');
+      $table->integer('status');
+      $table->integer('type')->default(1);
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('withdraw_queues');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('withdraw_queues');
+  }
 }
